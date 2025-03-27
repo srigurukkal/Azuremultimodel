821e9fcb4a576fde35745fa9d2ca05c4505b3960
sridhar eswaran <srigurukkal@hotmail.com>
2025-03-26 22:46:44 -0500

    bug fixes

--- ecovoice-app-backend/Program.cs
+++ ecovoice-app-backend/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", builder =>
     {
-        builder.WithOrigins("http://localhost:3000")
+        builder.WithOrigins("http://localhost:3000", "https://*.azurestaticapps.net")
                .AllowAnyMethod()
                .AllowAnyHeader()
                .AllowCredentials();
